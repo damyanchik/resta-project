144ca15c860a24b9fb8527d070a6df7e6ec3eaf7
damyanchik <d4mian.ndz@gmail.com>
2024-05-02 19:13:07 +0200

    implemented vue for frontend app

---
+++ resources/js/components/example.vue
@@ -0,0 +1,13 @@
+<template>
+ <h1>EXAMPLE</h1>
+</template>
+
+<script>
+export default {
+    name: "example"
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', fn() => view('index'));
+
 Route::post('/api/cart/add/{uuid}', \App\Components\Cart\Infrastructure\Http\Handler\AddItemCartHandler::class)
     ->name('cart.add');
 
